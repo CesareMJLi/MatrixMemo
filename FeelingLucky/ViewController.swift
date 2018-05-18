66f0995b4cec3d68f825a0d06ff30aee67a14043
Unknown <cesareli1995@gmail.com>
2018-05-18 16:55:24 +0200

    Initial

---
+++ FeelingLucky/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  FeelingLucky
+//
+//  Created by 李明举 on 2018/5/13.
+//  Copyright © 2018年 李明举. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var sendNudes: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        self.hideKeyboardWhenTappedAround()
+    }
+    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+    
+    // MARK: Actions
+    @IBAction func setNudes(_ sender: UIButton) {
+        if password.text == "1207"{
+            self.performSegue(withIdentifier:"toContentPage", sender:self)
+        } else {
+            sendNudes.text = "Send Nudes XD"
+        }
+    }
+}
+
+// Put this piece of code anywhere you like
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
